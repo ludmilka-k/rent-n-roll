a94804b909e592edb2bd44e812eac52696ea5a74
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-11-02 14:42:02 +0200

    Connect theme to the provider

---
+++ src/constants/theme.js
@@ -0,0 +1,10 @@
+
+export const theme= {
+  colors: {
+    primaryTextColor: '#212121',
+    secondaryTextColor: '#757575',
+    mainBGColor: '#F7F6F9',
+    mainAccentColor: '#3E85F3',
+    mainAccentColorActive: '#2B78EF',
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import App from './components/App';
+import { theme } from './constants/';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename='/rent-n-roll'>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
