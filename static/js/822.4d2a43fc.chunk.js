e70429f1c3244b458024c046fd5231d492a103d9
ludmilka-k <ludmilka.kurochkina@gmail.com>
2023-11-02 12:43:13 +0000

    Deploying to gh-pages from @ ludmilka-k/rent-n-roll@a94804b909e592edb2bd44e812eac52696ea5a74 🚀

---
+++ static/js/822.4d2a43fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrent_n_roll=self.webpackChunkrent_n_roll||[]).push([[822],{822:function(n,e,r){r.r(e);var t=r(184);e.default=function(){return(0,t.jsx)(t.Fragment,{})}}}]);
+//# sourceMappingURL=822.4d2a43fc.chunk.js.map
