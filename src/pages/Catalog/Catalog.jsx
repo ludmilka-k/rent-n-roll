6d743f4b7f73eee4c8063c59cad439457f4002b8
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-11-05 20:12:34 +0200

    fix warning

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import {fetchCarsThunk} from '../../redux/operations';
 import FilterBar from '../../components/FilterBar';
@@ -7,8 +7,6 @@ import { selectCars } from '../../redux/selectors';
 
 const Catalog = () => {
   const dispatch = useDispatch();
-  // const [cars, setCars] = useState([]);
-  const [page, setPage] = useState(1);
   const cars = useSelector(selectCars);
 
   useEffect(() => {
