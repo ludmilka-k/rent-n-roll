c5b885736a0772891d2432936c5915e7cd5a6b4a
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-11-13 15:38:57 +0200

    Add section as empty to Favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,11 +1,19 @@
 import CarsList from '../../components/CarsList';
 import { useSelector } from 'react-redux';
 import { selectFavorites } from '../../redux/selectors';
+import { Title, Link, Section } from '../Home/Home.styled';
 const Favorites = () => {
   const favoriteCars = useSelector(selectFavorites);
   return (
     <>
-      {favoriteCars.favorites && <CarsList cars={favoriteCars.favorites}/>}
+      {favoriteCars.favorites.length ? (
+        <CarsList cars={favoriteCars.favorites}/>
+      ) : (
+       <Section>
+         <Title>Your favorites are currently empty...</Title>
+         <Link to="/catalog">Go to Catalog</Link>
+       </Section>
+      )}
     </>
   );
 };
